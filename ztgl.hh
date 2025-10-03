5575b52c598aef844dd8382a2007a7c25ef497db
tirimid <tirimid@tirimid.net>
2025-10-03 09:32:51 +0400

    continue working

--- ztgl.hh
+++ ztgl.hh
@@ -57,7 +57,7 @@ extern "C"
 #define ZTGL_INC_RES(Name) \
 	{ \
 		.m_Data = Name, \
-		.m_Size = &Name##_len \
+		.m_Size = Name##_len \
 	}
 
 //--------------//
@@ -90,7 +90,7 @@ namespace ZTGL
 // enumeration values //
 //--------------------//
 
-enum ErrCode
+enum ErrorCode
 {
 	OK = 0,
 	NOT_FOUND,
@@ -132,38 +132,38 @@ enum Color
 	INACTIVE_COLOR
 };
 
-//-----------------//
-// data structures //
-//-----------------//
+//----------------------------//
+// standalone data structures //
+//----------------------------//
 
 struct Conf
 {
 	// library function config.
-	char const *m_ErrTitle;
-	FILE  *m_Log;
-	u64    m_TickUS;
-	i32    m_UIPad;
-	i32    m_UITextFieldBar;
+	char const* m_ErrorTitle;
+	FILE* m_Log;
+	u64   m_TickMicro;
+	i32   m_UIPad;
+	i32   m_UITextFieldBar;
 	
 	// rendering call config.
 	void (*m_RenderRect)(i32, i32, i32, i32, Color);
-	void (*m_RenderText)(i32, i32, i32, i32, char const *, Color);
+	void (*m_RenderText)(i32, i32, i32, i32, char const*, Color);
 };
 
 struct Res
 {
-	u8  *m_Data;
-	u32 *m_Size;
+	u8*  m_Data;
+	u32& m_Size;
 };
 
 struct TFData
 {
-	char *m_Buf;
-	u32   m_Len;
-	u32   m_Cap;
-	u32   m_Csr;
+	char* m_Buffer;
+	u32   m_Length;
+	u32   m_Capacity;
+	u32   m_Cursor;
 	u32   m_First;
-	bool  m_Sel;
+	bool  m_Selected;
 };
 
 union UIElem
@@ -187,7 +187,7 @@ union UIElem
 		i32 m_Y;
 		i32 m_W;
 		i32 m_H;
-		char const *m_Text;
+		char const* m_Text;
 	} m_Label;
 	
 	struct
@@ -198,7 +198,7 @@ union UIElem
 		i32 m_Y;
 		i32 m_W;
 		i32 m_H;
-		char const *m_Text;
+		char const* m_Text;
 	} m_Button;
 	
 	struct
@@ -209,8 +209,8 @@ union UIElem
 		i32 m_Y;
 		i32 m_W;
 		i32 m_H;
-		f32 m_Val;
-		char const *m_Text;
+		f32 m_Value;
+		char const* m_Text;
 	} m_Slider;
 	
 	struct
@@ -222,8 +222,8 @@ union UIElem
 		i32 m_W;
 		i32 m_H;
 		u32 m_NDraw;
-		char const   *m_Text;
-		TFData const *m_TFData;
+		char const*   m_Text;
+		TFData const& m_TFData;
 	} m_TextField;
 	
 	struct
@@ -234,38 +234,52 @@ union UIElem
 		i32 m_Y;
 		i32 m_W;
 		i32 m_H;
-		char const *m_Text;
+		char const* m_Text;
 	} m_HoldButton;
 };
 
+struct AllocBatchDesc
+{
+	void*& m_Pointer;
+	usize  m_Count;
+	usize  m_Size;
+};
+
+struct ReallocBatchDesc
+{
+	void*& m_Pointer;
+	usize  m_OldCount;
+	usize  m_NewCount;
+	usize  m_Size;
+};
+
+//------------------------------//
+// data structures with methods //
+//------------------------------//
+
+// ui.
 struct UIPanel
 {
-	UIElem   *m_Elems;
-	usize     m_NElems;
-	usize     m_ElemCap;
-	TTF_Font *m_Font;
-	SDL_Window const *m_Wnd;
+	UIElem*   m_Elems;
+	usize     m_ElemsLength;
+	usize     m_ElemsCapacity;
+	TTF_Font* m_Font;
+	SDL_Window const* m_Window;
 	
 	// can safely be modified by end user.
 	i32  m_X;
 	i32  m_Y;
 	bool m_Active;
 	bool m_Horizontal;
-};
-
-struct AllocBatchDesc
-{
-	void **m_Ptr;
-	usize  m_N;
-	usize  m_Size;
-};
-
-struct ReallocBatchDesc
-{
-	void **m_Ptr;
-	usize  m_NOld;
-	usize  m_NNew;
-	usize  m_Size;
+	
+	UIPanel(UIElem elems[], usize elemsCapacity, TTF_Font* font, SDL_Window const* window);
+	
+	void Render();
+	void Label(char const* text);
+	bool Button(char const* text);
+	bool Slider(char const* text, IN_OUT f32& value);
+	void TextField(char const* text, IN_OUT TFData& data, u32 nDraw);
+	bool HoldButton(char const* text);
 };
 
 //-----------------------//
@@ -285,37 +299,37 @@ extern u8 DefaultColors[][4];
 //-----------------------//
 
 // input.
-void HandleInput(SDL_Event const *e);
+void HandleInput(SDL_Event const& event);
 void PrepareInput();
-bool KDown(SDL_Keycode k);
-bool KPressed(SDL_Keycode k);
-bool KReleased(SDL_Keycode k);
-void MPos(SDL_Window const *wnd, OUT i32 *x, OUT i32 *y);
-bool MDown(i32 btn);
-bool MPressed(i32 btn);
-bool MReleased(i32 btn);
+bool KeyDown(SDL_Keycode key);
+bool KeyPressed(SDL_Keycode key);
+bool KeyReleased(SDL_Keycode key);
+SDL_Point MousePos(SDL_Window const* window);
+bool MouseDown(i32 button);
+bool MousePressed(i32 button);
+bool MouseReleased(i32 button);
 bool TextInput(char c);
 
 // options.
-ErrCode OptRaw(OUT char data[], FILE *f, char const *key);
-ErrCode OptKeycode(OUT SDL_Keycode *data, FILE *f, char const *key);
-ErrCode OptFloat(OUT f32 *data, FILE *f, char const *key);
-ErrCode OptInt(OUT i64 *data, FILE *f, char const *key);
-ErrCode OptBool(OUT bool *data, FILE *f, char const *key);
+ErrorCode OptionRaw(OUT char data[], FILE* file, char const* key);
+ErrorCode OptionKeycode(OUT SDL_Keycode& data, FILE* file, char const* key);
+ErrorCode OptionFloat(OUT f32& data, FILE* file, char const* key);
+ErrorCode OptionInt(OUT i64& data, FILE* file, char const* key);
+ErrorCode OptionBool(OUT bool& data, FILE* file, char const* key);
 
 // util.
-void  Err(char const *fmt, ...);
-u64   UnixUS();
+void  Error(char const* format, ...);
+u64   UnixMicro();
 void  BeginTick();
 void  EndTick();
-void  BeginTimer(OUT u64 *timer);
-void  EndTimer(u64 timer, char const *name);
+void  BeginTimer(OUT u64& timer);
+void  EndTimer(u64 timer, char const* name);
 f32   InterpAngle(f32 a, f32 b, f32 t);
 f32   ShortestAngle(f32 a, f32 b);
-f32   ToDeg(f32 r);
-f32   ToRad(f32 d);
-void *AllocBatch(IN_OUT AllocBatchDesc *allocs, usize nAllocs);
-void *ReallocBatch(void *p, IN_OUT ReallocBatchDesc *reallocs, usize nReallocs);
+f32   Degrees(f32 rad);
+f32   Radians(f32 deg);
+void* AllocBatch(IN_OUT AllocBatchDesc allocs[], usize nAllocs);
+void* ReallocBatch(void* p, IN_OUT ReallocBatchDesc reallocs[], usize nReallocs);
 u64   Align(u64 addr, u64 align);
 
 }
@@ -340,13 +354,27 @@ extern "C"
 namespace ZTGL::Internal
 {
 
+enum UIFlag
+{
+	INACTIVE = 0x1
+};
+
+enum UIType
+{
+	LABEL = 0,
+	BUTTON,
+	SLIDER,
+	TEXT_FIELD,
+	HOLD_BUTTON
+};
+
 // input.
-u8 KDownStates[1024 / 8];
-u8 KPressStates[1024 / 8];
-u8 KReleaseStates[1024 / 8];
-u8 MDownStates;
-u8 MPressStates;
-u8 MReleaseStates;
+u8 KeyDownStates[1024 / 8];
+u8 KeyPressStates[1024 / 8];
+u8 KeyReleaseStates[1024 / 8];
+u8 MouseDownStates;
+u8 MousePressStates;
+u8 MouseReleaseStates;
 u8 TextInputStates[128 / 8];
 
 // util.
@@ -362,102 +390,262 @@ namespace ZTGL
 //-------//
 
 void
-HandleInput(SDL_Event const *e)
+HandleInput(SDL_Event const& event)
 {
+	if (event.type == SDL_KEYDOWN || event.type == SDL_KEYUP)
+	{
+		if (event.key.repeat)
+		{
+			return;
+		}
+		
+		bool state = event.type == SDL_KEYDOWN;
+		
+		SDL_Keycode key = event.key.keysym.sym;
+		if (key & 1 << 30)
+		{
+			key &= ~(1 << 30);
+			key += 12;
+		}
+		
+		usize byte = key / 8;
+		usize bit = key % 8;
+		
+		if (state)
+		{
+			Internal::KeyDownStates[byte] |= 1 << bit;
+			Internal::KeyPressStates[byte] |= 1 << bit;
+		}
+		else
+		{
+			Internal::KeyDownStates[byte] &= ~(1 << bit);
+			Internal::KeyReleaseStates[byte] |= 1 << bit;
+		}
+	}
+	else if (event.type == SDL_MOUSEBUTTONDOWN || event.type == SDL_MOUSEBUTTONUP)
+	{
+		bool state = event.type == SDL_MOUSEBUTTONDOWN;
+		
+		if (state)
+		{
+			Internal::MouseDownStates |= 1 << event.button.button;
+			Internal::MousePressStates |= 1 << event.button.button;
+		}
+		else
+		{
+			Internal::MouseDownStates &= ~(1 << event.button.button);
+			Internal::MouseReleaseStates |= 1 << event.button.button;
+		}
+	}
+	else if (event.type == SDL_TEXTINPUT)
+	{
+		u8 c = event.text.text[0];
+		
+		// disregard non-ASCII input.
+		if (c & 0x80)
+		{
+			return;
+		}
+		
+		usize byte = c / 8;
+		usize bit = c % 8;
+		Internal::TextInputStates[byte] |= 1 << bit;
+	}
 }
 
 void
 PrepareInput()
 {
+	memset(Internal::KeyPressStates, 0, sizeof(Internal::KeyPressStates));
+	memset(Internal::KeyReleaseStates, 0, sizeof(Internal::KeyReleaseStates));
+	
+	Internal::MousePressStates = 0;
+	Internal::MouseReleaseStates = 0;
+	
+	memset(Internal::TextInputStates, 0, sizeof(Internal::TextInputStates));
 }
 
 bool
-KDown(SDL_Keycode k)
+KeyDown(SDL_Keycode key)
 {
+	if (key & 1 << 30)
+	{
+		key &= ~(1 << 30);
+		key += 128;
+	}
+	usize byte = key / 8;
+	usize bit = key % 8;
+	return Internal::KeyDownStates[byte] & 1 << bit;
 }
 
 bool
-KPressed(SDL_Keycode k)
+KeyPressed(SDL_Keycode key)
 {
+	if (key & 1 << 30)
+	{
+		key &= ~(1 << 30);
+		key += 128;
+	}
+	usize byte = key / 8;
+	usize bit = key % 8;
+	return Internal::KeyPressStates[byte] & 1 << bit;
 }
 
 bool
-KReleased(SDL_Keycode k)
+KeyReleased(SDL_Keycode key)
 {
+	if (key & 1 << 30)
+	{
+		key &= ~(1 << 30);
+		key += 128;
+	}
+	usize byte = key / 8;
+	usize bit = key % 8;
+	return Internal::KeyReleaseStates[byte] & 1 << bit;
 }
 
-void
-MPos(SDL_Window const *wnd, OUT i32 *x, OUT i32 *y)
+SDL_Point
+MousePos(SDL_Window const* window)
 {
+	if (SDL_GetMouseFocus() != window)
+	{
+		return {0, 0};
+	}
+	
+	i32 x;
+	i32 y;
+	SDL_GetMouseState(&x, &y);
+	
+	return {x, y};
 }
 
 bool
-MDown(i32 btn)
+MouseDown(i32 button)
 {
+	return !!(Internal::MouseDownStates & 1 << button);
 }
 
 bool
-MPressed(i32 btn)
+MousePressed(i32 button)
 {
+	return !!(Internal::MousePressStates & 1 << button);
 }
 
 bool
-MReleased(i32 btn)
+MouseReleased(i32 button)
 {
+	return !!(Internal::MouseReleaseStates & 1 << button);
 }
 
 bool
 TextInput(char c)
 {
+	usize byte = c / 8;
+	usize bit = c % 8;
+	return Internal::TextInputStates[byte] & 1 << bit;
 }
 
 //---------//
 // options //
 //---------//
 
-ErrCode
-OptRaw(OUT char data[], FILE *f, char const *key)
+ErrorCode
+OptionRaw(OUT char data[], FILE* file, char const* key)
 {
+	(void)data, (void)file, (void)key;
+	__builtin_unreachable();
 }
 
-ErrCode
-OptKeycode(OUT SDL_Keycode *data, FILE *f, char const *key)
+ErrorCode
+OptionKeycode(OUT SDL_Keycode& data, FILE* file, char const* key)
 {
+	(void)data, (void)file, (void)key;
+	__builtin_unreachable();
 }
 
-ErrCode
-OptFloat(OUT f32 *data, FILE *f, char const *key)
+ErrorCode
+OptionFloat(OUT f32& data, FILE* file, char const* key)
 {
+	(void)data, (void)file, (void)key;
+	__builtin_unreachable();
 }
 
-ErrCode
-OptInt(OUT i64 *data, FILE *f, char const *key)
+ErrorCode
+OptionInt(OUT i64& data, FILE* file, char const* key)
 {
+	(void)data, (void)file, (void)key;
+	__builtin_unreachable();
 }
 
-ErrCode
-OptBool(OUT bool *data, FILE *f, char const *key)
+ErrorCode
+OptionBool(OUT bool& data, FILE* file, char const* key)
 {
+	(void)data, (void)file, (void)key;
+	__builtin_unreachable();
 }
 
 //----//
 // ui //
 //----//
 
+UIPanel::UIPanel(
+	UIElem elems[],
+	usize elemsCapacity,
+	TTF_Font* font,
+	SDL_Window const* window
+)
+	: m_Elems(elems),
+	m_ElemsCapacity(elemsCapacity),
+	m_Font(font),
+	m_Window(window),
+	m_Active(true)
+{
+}
+
+void
+UIPanel::Render()
+{
+}
+
+void
+UIPanel::Label(char const* text)
+{
+}
+
+bool
+UIPanel::Button(char const* text)
+{
+}
+
+bool
+UIPanel::Slider(char const* text, IN_OUT f32& value)
+{
+}
+
+void
+UIPanel::TextField(char const* text, IN_OUT TFData& data, u32 nDraw)
+{
+}
+
+bool
+UIPanel::HoldButton(char const* text)
+{
+}
+
 //------//
 // util //
 //------//
 
 void
-Err(char const *fmt, ...)
+Error(char const* format, ...)
 {
 	va_list args;
-	va_start(args, fmt);
+	va_start(args, format);
 	
 	char msg[512];
-	vsnprintf(msg, sizeof(msg), fmt, args);
+	vsnprintf(msg, sizeof(msg), format, args);
 	
-	if (SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, Conf.m_ErrTitle, msg, nullptr))
+	if (SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_ERROR, Conf.m_ErrorTitle, msg, nullptr))
 	{
 		fprintf(Conf.m_Log, "\x1b[1;31merr\x1b[0m: %s\n", msg);
 	}
@@ -466,7 +654,7 @@ Err(char const *fmt, ...)
 }
 
 u64
-UnixUS()
+UnixMicro()
 {
 	timeval tv;
 	gettimeofday(&tv, nullptr);
@@ -476,28 +664,35 @@ UnixUS()
 void
 BeginTick()
 {
-	Internal::TickStart = UnixUS();
+	Internal::TickStart = UnixMicro();
 }
 
 void
 EndTick()
 {
-	u64 tickEnd = UnixUS();
-	i64 timeLeft = Conf.m_TickUS - tickEnd + Internal::TickStart;
+	u64 tickEnd = UnixMicro();
+	i64 timeLeft = Conf.m_TickMicro - tickEnd + Internal::TickStart;
 	timeLeft *= timeLeft > 0;
 	
 	SDL_Delay(timeLeft / 1000);
 }
 
 void
-BeginTimer(OUT u64 *timer)
+BeginTimer(OUT u64& timer)
 {
-	*timer = UnixUS();
+	timer = UnixMicro();
 }
 
 void
-EndTimer(u64 timer, char const *name)
+EndTimer(u64 timer, char const* name)
 {
+	u64 d = UnixMicro() - timer;
+	fprintf(
+		Conf.m_Log,
+		"\x1b[1;33mprofile\x1b[0m: %s: %llu\n",
+		name,
+		(unsigned long long)d
+	);
 }
 
 f32
@@ -515,37 +710,59 @@ ShortestAngle(f32 a, f32 b)
 }
 
 f32
-ToDeg(f32 r)
+Degrees(f32 rad)
 {
-	return r / PI * 180.0f;
+	return rad / PI * 180.0f;
 }
 
 f32
-ToRad(f32 d)
+Radians(f32 deg)
 {
-	return d / 180.0f * PI;
+	return deg / 180.0f * PI;
 }
 
-void *
-AllocBatch(IN_OUT AllocBatchDesc *allocs, usize nAllocs)
+void*
+AllocBatch(IN_OUT AllocBatchDesc allocs[], usize nAllocs)
 {
+	usize size = 0;
+	for (usize i = 0; i < nAllocs; ++i)
+	{
+		size += allocs[i].m_Count * allocs[i].m_Size;
+		size = Align(size, ZTGL_BATCH_ALIGN);
+	}
+	
+	u8* p = (u8*)malloc(size);
+	if (!p)
+	{
+		return nullptr;
+	}
+	
+	usize offset = 0;
+	for (usize i = 0; i < nAllocs; ++i)
+	{
+		allocs[i].m_Pointer = &p[offset];
+		offset += allocs[i].m_Count * allocs[i].m_Size;
+		offset = Align(offset, ZTGL_BATCH_ALIGN);
+	}
+	
+	return p;
 }
 
-void *
-ReallocBatch(void *p, IN_OUT ReallocBatchDesc *reallocs, usize nReallocs)
+void*
+ReallocBatch(void* p, IN_OUT ReallocBatchDesc reallocs[], usize nReallocs)
 {
-	usize *oldOffsets = (usize *)calloc(nReallocs, sizeof(usize));
-	usize *newOffsets = (usize *)calloc(nReallocs, sizeof(usize));
+	usize* oldOffsets = (usize*)calloc(nReallocs, sizeof(usize));
+	usize* newOffsets = (usize*)calloc(nReallocs, sizeof(usize));
 	
 	usize newSize = 0, oldSize = 0;
 	for (usize i = 0; i < nReallocs; ++i)
 	{
 		newOffsets[i] = newSize;
-		newSize += reallocs[i].m_NNew * reallocs[i].m_Size;
+		newSize += reallocs[i].m_NewCount * reallocs[i].m_Size;
 		newSize = Align(newSize, ZTGL_BATCH_ALIGN);
 		
 		oldOffsets[i] = oldSize;
-		oldSize += reallocs[i].m_NOld * reallocs[i].m_Size;
+		oldSize += reallocs[i].m_OldCount * reallocs[i].m_Size;
 		oldSize = Align(oldSize, ZTGL_BATCH_ALIGN);
 	}
 	
@@ -557,15 +774,15 @@ ReallocBatch(void *p, IN_OUT ReallocBatchDesc *reallocs, usize nReallocs)
 		return nullptr;
 	}
 	
-	u8 *up = (u8 *)p;
+	u8* up = (u8*)p;
 	for (isize i = nReallocs - 1; i >= 0; --i)
 	{
-		usize newBytes = reallocs[i].m_NNew * reallocs[i].m_Size;
-		usize oldBytes = reallocs[i].m_NOld * reallocs[i].m_Size;
+		usize newBytes = reallocs[i].m_NewCount * reallocs[i].m_Size;
+		usize oldBytes = reallocs[i].m_OldCount * reallocs[i].m_Size;
 		usize bytes = newBytes < oldBytes ? newBytes : oldBytes;
 		
 		memmove(&up[newOffsets[i]], &up[oldOffsets[i]], bytes);
-		*reallocs[i].m_Ptr = &up[newOffsets[i]];
+		reallocs[i].m_Pointer = &up[newOffsets[i]];
 	}
 	
 	free(oldOffsets);
