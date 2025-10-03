dfd35a23e78cb78bd28a8688e224079aa57f6775
tirimid <tirimid@tirimid.net>
2025-10-03 16:59:34 +0400

    default color list

--- ztgl.hh
+++ ztgl.hh
@@ -281,7 +281,7 @@ struct UIPanel
 // library configuration //
 //-----------------------//
 
-extern Conf conf;
+extern Conf conf; // must be initialized prior to use.
 
 //-------------//
 // data tables //
@@ -381,6 +381,42 @@ u64 tickStart;
 
 namespace ZTGL
 {
+
+Conf conf;
+
+u8 defaultColors[][4] =
+{
+	{0, 0, 0, 128}, // panel.
+	{255, 255, 255, 255}, // label text.
+	{255, 255, 255, 128}, // button.
+	{0, 0, 0, 255}, // button press.
+	{128, 128, 128, 255}, // button hover.
+	{255, 255, 255, 255}, // button text.
+	{255, 255, 255, 255}, // button press text.
+	{255, 255, 255, 255}, // button hover text.
+	{0, 0, 0, 128}, // slider.
+	{0, 0, 0, 255}, // slider press.
+	{0, 0, 0, 255}, // slider hover.
+	{128, 128, 128, 255}, // slider bar.
+	{128, 128, 128, 255}, // slider press bar.
+	{128, 128, 128, 255}, // slider hover bar.
+	{255, 255, 255, 255}, // slider text.
+	{255, 255, 255, 255}, // slider press text.
+	{255, 255, 255, 255}, // slider hover text.
+	{0, 0, 0, 128}, // textfield.
+	{0, 0, 0, 255}, // textfield press.
+	{0, 0, 0, 255}, // textfield hover.
+	{255, 255, 255, 255}, // textfield text.
+	{255, 255, 255, 255}, // textfield press text.
+	{255, 255, 255, 255}, // textfield hover text.
+	{255, 255, 255, 255}, // textfield bar.
+	{255, 255, 255, 255}, // textfield press bar.
+	{255, 255, 255, 255}, // textfield hover bar.
+	{128, 128, 128, 255}, // textfield prompt.
+	{128, 128, 128, 255}, // textfield press prompt.
+	{128, 128, 128, 255}, // textfield hover prompt.
+	{0, 0, 0, 128} // inactive.
+};
 
 //-------//
 // input //
